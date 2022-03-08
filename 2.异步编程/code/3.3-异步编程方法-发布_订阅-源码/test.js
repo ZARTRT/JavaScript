29eaad878a956e5f1c6b7228d7a0fe147418a92d
v_jninzhang <v_jninzhang@tencent.com>
2022-03-08 23:56:37 +0800

    js(subscribe): 发布订阅

--- 2.异步编程/code/3.3-异步编程方法-发布_订阅-源码/test.js
+++ 2.异步编程/code/3.3-异步编程方法-发布_订阅-源码/test.js
@@ -1,40 +1,15 @@
-function ajax(url, callback) {
-    // 实现省略
-}
-ajax("./test1.json", function(data) {
-    console.log(data);
-    ajax("./test2.json", function(data) {
-        console.log(data);
-        ajax("./test3.json", function(data) {
-            console.log(data);
-        });
-    });
-});
-
-// 发布订阅应用
-function ajax(url, callback) {
-    // 实现省略
-}
-
-const pbb = new PubSub();
-ajax("./test1.json", function(data) {
-    pbb.publish("test1Success", data);
-});
-pbb.subscribe("test1Success", function(data) {
-    console.log(data);
-    ajax("./test2.json", function(data) {
-        pbb.publish("test2Success", data);
-    });
-});
-pbb.subscribe("test2Success", function(data) {
-    console.log(data);
-    ajax("./test3.json", function(data) {
-        pbb.publish("test3Success", data);
-    });
-});
-pbb.subscribe("test2Success", function(data) {
-    console.log(data);
-});
+// function ajax(url, callback) {
+//     // 实现省略
+// }
+// ajax("./test1.json", function(data) {
+//     console.log(data);
+//     ajax("./test2.json", function(data) {
+//         console.log(data);
+//         ajax("./test3.json", function(data) {
+//             console.log(data);
+//         });
+//     });
+// });
 
 class PubSub {
     constructor() {
@@ -62,3 +37,32 @@ class PubSub {
         }
     }
 }
+
+// 发布订阅应用
+function ajax(url, callback) {
+    // 实现省略
+}
+
+const pbb = new PubSub();
+ajax("./test1.json", function(data) {
+    pbb.publish("test1Success", data);
+});
+pbb.subscribe("test1Success", function(data) {
+    console.log(data);
+    ajax("./test2.json", function(data) {
+        pbb.publish("test2Success", data);
+    });
+});
+pbb.subscribe("test2Success", function(data) {
+    console.log(data);
+    ajax("./test3.json", function(data) {
+        pbb.publish("test3Success", data);
+    });
+});
+pbb.subscribe("test2Success", function(data) {
+    console.log(data);
+});
+
+console.log(pbb.events);
+
+

c05ff78c061502004f86ab667f30ecd9a755d5ac
v_jninzhang <v_jninzhang@tencent.com>
2022-03-11 13:47:22 +0800

    js: async && await

--- 2.异步编程/code/3.4-深入理解promise/promise顺序执行.js
+++ 2.异步编程/code/3.4-深入理解promise/promise顺序执行.js
@@ -8,30 +8,66 @@
 //     console.log(3);
 // }, 3000);
 
-let promise3 = Promise.resolve();
-promise3 = promise3.then(function () {
-   return new Promise(function (resolve,reject) {
-        setTimeout(() => {
-            console.log(1);
-            resolve();
-        }, 3000);
-    })
-})
+// let promise3 = Promise.resolve();
+// promise3 = promise3.then(function () {
+//    return new Promise(function (resolve,reject) {
+//         setTimeout(() => {
+//             console.log(1);
+//             resolve();
+//         }, 3000);
+//     })
+// })
 
-promise3 = promise3.then(function () {
-    return new Promise(function (resolve,reject) {
-         setTimeout(() => {
-             console.log(2);
-             resolve();
-         }, 2000);
-     })
- })
+// promise3 = promise3.then(function () {
+//     return new Promise(function (resolve,reject) {
+//          setTimeout(() => {
+//              console.log(2);
+//              resolve();
+//          }, 2000);
+//      })
+//  })
 
- promise3 = promise3.then(function () {
-    return new Promise(function (resolve,reject) {
-         setTimeout(() => {
-             console.log(3);
-             resolve();
-         }, 1000);
-     })
- })
+//  promise3 = promise3.then(function () {
+//     return new Promise(function (resolve,reject) {
+//          setTimeout(() => {
+//              console.log(3);
+//              resolve();
+//          }, 1000);
+//      })
+//  })
+
+// .then方法优化如下
+// let i = 1;
+// let perform = function (n) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//       console.log(i++);
+//       resolve();
+//     }, n * 1000);
+//   });
+// };
+// perform(3)
+//   .then(function (data) {
+//     return perform(2);
+//   })
+//   .then(function (data) {
+//     return perform(1);
+//   })
+//   .then(function (data) {});
+
+// async await结合
+async function func() {
+  let i = 1;
+  function perform(n) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(() => {
+        console.log(i++);
+        resolve();
+      }, n * 1000);
+    });
+  }
+  await perform(3);
+  await perform(2);
+  await perform(1);
+}
+func();
